43b9fa7b816b8d24da8f9fe51bc3d3a960de6524
Siddharth <siddharthmagadum16@gmail.com>
2020-08-05 01:01:26 +0530

    database part

--- src/Components/Docs/Docs.js
+++ src/Components/Docs/Docs.js
@@ -39,11 +39,31 @@ const Docs=()=>{
                         <p>Go</p>
                     </a>
                     <a className='elementd' href='https://swift.org/documentation/#the-swift-programming-language' target="_blank" rel="noopener noreferrer">
-                        <img  src='http://img.deusm.com/informationweek/2015/09/1322066/Swift_logo.png' alt='Go'/>
-                        <p>Go</p>
+                        <img  src='http://img.deusm.com/informationweek/2015/09/1322066/Swift_logo.png' alt='Swift'/>
+                        <p>Swift</p>
+                    </a>
+                    <a className='elementd' href='https://kotlinlang.org/docs/reference/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/1200px-Kotlin-logo.svg.png' alt='Kotlin'/>
+                        <p>Kotlin</p>
+                    </a>
+                    <a className='elementd' href='https://www.typescriptlang.org/docs/home.html' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://seeklogo.com/images/T/typescript-logo-B29A3F462D-seeklogo.com.png' alt='typescript'/>
+                        <p>Typescript</p>
+                    </a>
+                    <a className='elementd' href='https://www.php.net/docs.php' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/711px-PHP-logo.svg.png' alt='php'/>
+                        <p>PHP</p>
+                    </a>
+                    <a className='elementd' href='https://www.rust-lang.org/learn' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Rust_programming_language_black_logo.svg/1200px-Rust_programming_language_black_logo.svg.png' alt='Rust'/>
+                        <p>Rust</p>
+                    </a>
+                    <a className='elementd' href='https://www.gnu.org/software/bash/manual/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://raw.githubusercontent.com/odb/official-bash-logo/master/assets/Logos/Icons/PNG/512x512.png' alt='Bash'/>
+                        <p>Bash</p>
                     </a>
                 </div>
-                <p className='headingd'>Web Development</p>
+                <p className='headingd'>Web Frameworks</p>
                 <div className='gridd'>
                     <a className='elementd' href='https://nodejs.org/en/docs/' target="_blank" rel="noopener noreferrer">
                         <img  src='https://www.aca.am/assets/img/nodejs/node-js-icon.png' alt='nodejs'/>
@@ -57,6 +77,10 @@ const Docs=()=>{
                         <img  src='https://nedbatchelder.com/pix/django-icon-256.png' alt='django'/>
                         <p>Django</p>
                     </a>
+                    <a className='elementd' href='https://api.jquery.com/' target="_blank" rel="noopener noreferrer">
+                        <img  src='http://pluspng.com/img-png/logo-jquery-png--400.png' alt='jQuery'/>
+                        <p>jQuery</p>
+                    </a>
                     <a className='elementd' href='https://reactjs.org/docs/getting-started.html' target="_blank" rel="noopener noreferrer">
                         <img  src='https://cdn.idevie.com/wp-content/uploads/2015/12/React.js_logo.svg_.png' alt='react'/>
                         <p>React</p>
@@ -69,13 +93,67 @@ const Docs=()=>{
                         <img  src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png' alt='angular'/>
                         <p>Angular</p>
                     </a>
+                    <a className='elementd' href='https://docs.microsoft.com/en-us/aspnet/core/?view=aspnetcore-3.1' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://nextbigtechnology.com/wp-content/uploads/2018/12/asp-net-logo.png' alt='ASP.NET'/>
+                        <p>ASP.NET</p>
+                    </a>
+                    <a className='elementd' href='https://flask.palletsprojects.com/en/1.1.x/#user-s-guide' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://websitehostreview.com/wp-content/uploads/2015/06/flask.png' alt='Flask'/>
+                        <p>Flask</p>
+                    </a>
 
+                    <a className='elementd' href='https://expressjs.com/en/guide/routing.html' target="_blank" rel="noopener noreferrer">
+                        <img  src='http://alexgordienko.com/images/expressjslogo.png' alt='express'/>
+                        <p>Express.js</p>
+                    </a>
+                    <a className='elementd' href='http://guides.emberjs.com/release/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://emberjs.com/images/brand/ember_E-Icon-4c-Square.png' alt='ember'/>
+                        <p>Ember.js</p>
+                    </a>
                 </div>
+                <p className='headingd'>Databases</p>
+                <div className='gridd'>
+                    <a className='elementd' href='https://docs.mongodb.com/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://s3.amazonaws.com/static.tumblr.com/lbtm3t2/8PAn0kziu/mongodb-logo.png' alt='MongoDB'/>
+                        <p>MongoDB</p>
+                    </a>
+                    <a className='elementd' href='https://www.postgresql.org/docs/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png' alt='PostgreSQL'/>
+                        <p>PostgreSQL</p>
+                    </a>
+                    <a className='elementd' href='https://redis.io/documentation' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://seeklogo.com/images/R/redis-logo-E403D4DD6A-seeklogo.com.png' alt='Redis'/>
+                        <p>Redis</p>
+                    </a>
+                    <a className='elementd' href='https://www.elastic.co/guide/index.html' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://seeklogo.com/images/E/elasticsearch-logo-C75C4578EC-seeklogo.com.png' alt='elasticsearch'/>
+                        <p>Elasticsearch</p>
+                    </a>
+                    <a className='elementd' href='https://firebase.google.com/docs/guides?hl=en' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://coronalabs.com/wordpress/wp-content/uploads/2017/04/Firebase_16-logo.png' alt='Firebase'/>
+                        <p>Firebase</p>
+                    </a>
+                    <a className='elementd' href='https://dev.mysql.com/doc/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://seeklogo.com/images/M/mysql-logo-69B39F7D18-seeklogo.com.png' alt='MySQL'/>
+                        <p>MySQL</p>
+                    </a>
+                    <a className='elementd' href='https://www.sqlite.org/docs.html' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://librelamp.com/images/MariaDB-seal-plainsvgLG.svg' alt='SQLite'/>
+                        <p>SQLite</p>
+                    </a>
+                    <a className='elementd' href='https://mariadb.org/documentation/' target="_blank" rel="noopener noreferrer">
+                        <img  src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Sqlite-square-icon.svg/768px-Sqlite-square-icon.svg.png' alt='MariaDB'/>
+                        <p>MariaDB</p>
+                    </a>
+                </div>
+
+
             </div>
         </div>
 
     )
 }
-
-
+// https://codeforces.com/problemset/problem/496/C
+// https://insights.stackoverflow.com/survey/2020#technology-most-loved-dreaded-and-wanted-databases-loved4
+// get all the coding site at one place
 export default Docs;
