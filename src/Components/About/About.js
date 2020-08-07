75305fe999a62b59e36170b1c923af68221d185a
Siddharth <siddharthmagadum16@gmail.com>
2020-08-07 09:32:13 +0530

    completed2

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,7 @@ const About=()=>{
                 Are you tired of spending your time searching most used programming websites daily?
                 Then you are at the right place.<br/>
                 {/* <img src='https://www.lifewire.com/thmb/_-mt3H14b1_UeeFE1e6zQTljLYo=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-481668757-5b30f1ff8e1b6e00368c6b2d.jpg'></img> */}
-                <img src='https://i.pinimg.com/736x/87/59/67/87596735373c5744ff98ea28c1caac36.jpg'></img>
+                <img src='https://i.pinimg.com/736x/87/59/67/87596735373c5744ff98ea28c1caac36.jpg' alt='search'></img>
                 <br/> Code-in-One gives all the programming websites ready in a single click.
                 Most used programming sites are listed in home page and the official documentation of frameworks and programming languages
                  can be found at the documentation part of the website.<br/>You could just bookmark or pin a single website to get all the websites readily accessible.
